5b08ae4b0f344b231797efd3ee5697ac3a8acf6f
Kulonbekov <kulonbekov@gmail.com>
2023-05-15 23:00:54 +0600

    made edits in two classes, added the @entity annotation and the Id field

--- RealEstate/src/main/java/com/company/RealEstate/models/entities/User.java
+++ RealEstate/src/main/java/com/company/RealEstate/models/entities/User.java
@@ -1,4 +1,14 @@
 package com.company.RealEstate.models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
