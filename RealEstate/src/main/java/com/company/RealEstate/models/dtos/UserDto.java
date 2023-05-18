ba2b8058d1f3b8559fa7d6a1f5e930072d2e865c
Kulonbekov <kulonbekov@gmail.com>
2023-05-18 15:34:22 +0600

    Update UserDto

--- RealEstate/src/main/java/com/company/RealEstate/models/dtos/UserDto.java
+++ RealEstate/src/main/java/com/company/RealEstate/models/dtos/UserDto.java
@@ -2,9 +2,18 @@ package com.company.RealEstate.models.dtos;
 
 import com.company.RealEstate.models.entities.Role;
 import com.company.RealEstate.models.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto extends BaseDto{
 
     String username;
